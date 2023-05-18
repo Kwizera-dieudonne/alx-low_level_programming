252da0f4661d31f435f270e403b3102012256c45
kwizera dieudonne <kwizerad200@gmail.com>
2023-05-18 11:41:19 -0700

    c hello world tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main -a program that print message with puts
+ * Return: 0 successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
