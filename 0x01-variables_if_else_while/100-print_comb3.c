856ef9f2ff6f9e3579c59389175aa1c6afc63145
kwizera dieudonne <kwizerad200@gmail.com>
2023-06-06 15:41:08 -0700

    variable if else ehile

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - include possible combination
+ * of two digit
+ * Return: 0 always
+ */
+int main(void)
+{
+
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print all possible con of single
+ *digit
+ * Return: 0 successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
